b2a2f438b2ee13485b166f234cb152584ae5114e
enochndika <enochbeloved@gmail.com>
2021-07-05 22:41:54 +0100

    add global title and documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      width: {
-        99: 'calc(100% - 16rem);' /* get the width of the main content from lg:viewport by dividing
-        (the total width by the width of the side navigation)*/,
-      },
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
